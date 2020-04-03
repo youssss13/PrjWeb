d2653e4f7bbe97145d1920bfbfcabe505a6ce328
youssss13 <56067970+youssss13@users.noreply.github.com>
2020-04-03 21:00:33 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+"use strict"
+/* Serveur pour le site de recettes */
+var express = require('express');
+var mustache = require('mustache-express');
+
+var app = express();
+
+app.engine('html', mustache());
+app.set('view engine', 'html');
+app.set('views', './views');
+
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(3000, () => console.log('listening on http://localhost:3000'));
