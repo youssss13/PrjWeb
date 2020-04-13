e75f15b380b37b60ffc0fb962cc6391b717a095b
Errik <youssef_13150@hotmail.fr>
2020-04-14 00:18:17 +0200

    roots + views

--- server.js
+++ server.js
@@ -8,9 +8,35 @@ app.engine('html', mustache());
 app.set('view engine', 'html');
 app.set('views', './views');
 
+var url = require('url');
+var querystring = require('querystring');
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  res.render('index');
+  var params = querystring.parse(url.parse(req.url).query);
+  if('search' in params)
+  {
+    //profils exemples en attendant la mise en place de la base de données
+    res.render('search',{person: [{id : 1, name: 'DJ Khaled', birthdate : "05-09-1998", description : "gneugneugneu", register : "02-03-2020"}, {id : 2, name: 'Con Finés', birthdate : "05-09-1998", description : "gneugneugneu", register : "02-03-2020"},{id : 3, name: 'Jean Sorpa', birthdate : "05-09-1998", description : "gneugneugneu", register : "02-03-2020"}]})
+  }
+  else if('profile' in params)
+  {
+    res.render('profile',{id : params.profile});
+  }
+  else
+  {
+    res.render('index');
+  }
 });
+
+app.use(function(req, res, next){
+  res.setHeader('Content-Type', 'text/plain');
+  res.status(404).send('Page introuvable !');
+});
+
+
+
+
 
 app.listen(3000, () => console.log('listening on http://localhost:3000'));
